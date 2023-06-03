f82f82254b2ee06a9058041285dd9a5a03c6dfc3
Gilmar Alberto <gilabreu00@gmail.com>
2023-06-03 07:21:32 -0300

    desafios

---
+++ desafios/desafio001/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio001</title>
+</head>
+
+<body>
+    <h1>Teste de Tipos Primitivos</h1>
+
+    <?php 
+            /*
+            1. Crie uma variável do tipo inteiro e atribua um valor a ela. Em seguida, exiba o valor dessa variável na tela.
+
+            2. Declare uma variável do tipo float e atribua um valor decimal a ela. Em seguida, exiba o valor dessa variável na tela.
+
+            3. Crie uma variável do tipo string e atribua um texto a ela. Em seguida, exiba o conteúdo dessa variável na tela.
+
+            4. Declare uma variável do tipo booleano e atribua a ela um valor verdadeiro ou falso. Em seguida, exiba o valor dessa variável na tela.
+
+            5. Crie uma variável do tipo nulo e exiba o seu valor na tela.
+            */
+            
+            //1. Inteiro
+            $num = 100;
+            echo "O valor da variável inteira é $num<br>";
+
+            //2. Float
+            $num = 99.99;
+            echo "O valor da variável float é $num<br>";
+
+            //3. Strig
+
+            $nome = "Gilmar";
+
+            echo "O valor da variável string é $nome<br>";
+
+            //4. Booleana
+            $logica = true;
+
+            echo "O valor da variável booleana é $logica<br>";
+
+            //5. Null
+
+            $nulo = null;
+            echo "o valor da variavel null é $nulo<br>";
+
+        ?>
+
+</body>
+
+</html>
